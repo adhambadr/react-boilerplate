228197197baf7bf65559a8e36d27548cb2a52af1
adhambadr2@gmail.com <adhambadr2@gmail.com>
2017-04-23 00:55:07 +0200

    Updated dependancies, added loaders, added dist folder for index

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,33 @@
 var webpack = require('webpack');
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
     './src/index.jsx'
   ],
+    debug:true,
+  devtool:'#inline-source-map',
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
-    }]
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    {
+     test:/\.(png|woff|ttf|eot)$/, loader: 'url-loader?limit=100000'
+    },
+    {
+      test:/\.svg$/, loader :'svg-url-loader'
+    },
+    {
+        test: /\.json$/,
+        loader: 'json-loader'
+    }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
